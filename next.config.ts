5cd31a25656e36a72ed96ed7fec9a20086d93571
xBalzerian <balzgaming77@gmail.com>
2026-03-12 21:25:12 +0800

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compress: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/xBalzerian/strainhub/**",
+      },
+      { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "tempfile.aiquickdraw.com" },
     ],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24 * 30,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  experimental: {
+    optimizeCss: true,
   },
 };
 
